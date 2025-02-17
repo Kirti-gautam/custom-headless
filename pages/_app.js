d3e9330cc17d74ad7c8f7c504174b534187805f5
kirti.gautam <kirti.gautam@gokwik.co>
2025-02-17 20:59:37 +0530

    updated script

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '@/styles/globals.css'
-
+import "@/styles/globals.css";
+import Script from "next/script";
 export default function App({ Component, pageProps }) {
-
-  return <>
-  <Component {...pageProps} /> </>
+  return (
+    <>
+      <Script
+        src="https://sandbox.pdp.gokwik.co/kwikpass/plugin/build/kp-custom-merchant-v2.js"
+        strategy="afterInteractive"
+        onLoad={() => {
+          console.log("kp script loaded");
+          window.dispatchEvent(new Event("kp_script_loaded"));
+        }}
+      />
+      <Component {...pageProps} />{" "}
+    </>
+  );
 }
