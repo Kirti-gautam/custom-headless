984b422da334cf3095c76aff419abafd310d0aba
kirti.gautam <kirti.gautam@gokwik.co>
2025-02-18 00:07:37 +0530

    fix(fed): handle page view events for custom healess

--- pages/_app.js
+++ pages/_app.js
@@ -7,36 +7,35 @@ export default function App({ Component, pageProps }) {
   const router = useRouter();
 
   useEffect(() => {
-    if (isScriptLoaded && typeof window !== "undefined") {
-      console.log(
-        "inside use effect -> send page view event -> app.js",
-        window.__KP_LOGIN_SDK_INSTANCE__
-      );
-      const collectionPageViewEvent = new CustomEvent("page_view_kp", {
-        detail: {
-          type: "collection",
-          data: {
-            handle: `${router.pathname}`,
-            name: "product-category",
-            collection_id: "45678765456",
-            img_url:
-              "https://d20ce51t0ju3pt.cloudfront.net/images/single-product-images/fast-up-fusion-tech-protein-elevate-clinically-tested-for-2x-faster-absorption-1-1528_1692704801.webp",
-          },
-        },
-      });
-
-      window.dispatchEvent(collectionPageViewEvent);
-    }
-  }, [isScriptLoaded, router.pathname]);
+    window.kpqueue.push({
+      type: "collection",
+      data: {
+        handle: `${router.pathname}`,
+        name: "product-category",
+        collection_id: "45678765456",
+        img_url:
+          "https://d20ce51t0ju3pt.cloudfront.net/images/single-product-images/fast-up-fusion-tech-protein-elevate-clinically-tested-for-2x-faster-absorption-1-1528_1692704801.webp",
+      },
+    });
+  }, []);
 
   return (
     <>
       <Script
-        src="https://sandbox.pdp.gokwik.co/kwikpass/plugin/build/kp-custom-merchant-v2.js"
-        strategy="afterInteractive"
-        onLoad={() => {
-          console.log("kp script loaded");
-          setIsScriptLoaded(true);
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function () {
+              // add a queue event here
+              window.kpqueue = window.kpqueue || [];
+              function loadEvents(){ window.kpqueue.push(arguments)}
+              var s = document.createElement('script');
+              s.type = 'text/javascript';
+              s.async = true;
+              s.src = 'https://sandbox.pdp.gokwik.co/kwikpass/plugin/build/kp-custom-merchant-v2.js';
+              var x = document.getElementsByTagName('script')[0];
+              x.parentNode.insertBefore(s, x);
+            })();
+          `,
         }}
       />
       <Component {...pageProps} />{" "}
