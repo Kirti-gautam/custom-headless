45ffd2e08c0def696a9b5da837453893a08511d6
kirti.gautam <kirti.gautam@gokwik.co>
2025-08-12 23:50:02 +0530

    added meta events handling

--- pages/_app.js
+++ pages/_app.js
@@ -6,19 +6,6 @@ export default function App({ Component, pageProps }) {
   const [isScriptLoaded, setIsScriptLoaded] = useState(false);
   const router = useRouter();
 
-  useEffect(() => {
-    window.__KP_LOGIN_SDK_INSTANCE__.logEvents({
-      type: "collection",
-      data: {
-        handle: `${router.pathname}`,
-        name: "product-category",
-        collection_id: "45678765456",
-        img_url:
-          "https://d20ce51t0ju3pt.cloudfront.net/images/single-product-images/fast-up-fusion-tech-protein-elevate-clinically-tested-for-2x-faster-absorption-1-1528_1692704801.webp",
-      },
-    });
-  }, [router.pathname]);
-
   return (
     <>
       <Script
@@ -32,8 +19,8 @@ export default function App({ Component, pageProps }) {
               };
               var s = document.createElement('script');
               s.type = 'text/javascript';
-              s.async = true;
-              s.src = 'https://sandbox.pdp.gokwik.co/kwikpass/plugin/build/kp-custom-merchant-v2.js';
+              s.defer = true;
+              s.src = 'http://sandbox.pdp.gokwik.co/kwikpass/plugin/build/kp-custom-merchant.js';
               var x = document.getElementsByTagName('script')[0];
               x.parentNode.insertBefore(s, x);
             })();
--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,33 @@ import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Facebook Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '301126941903587');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=301126941903587&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </Head>
       <body>
         <Main />
         <NextScript />
@@ -14,7 +40,7 @@ export default function Document() {
             __html: `
               window.merchantInfo = {
                 environment: "sandbox",
-                mid: "19mdug4trxwh",
+                mid: "19v4an5c5p45",
                 type: "merchantInfo"
               };
             `,
